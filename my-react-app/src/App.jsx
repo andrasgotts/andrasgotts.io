4486fdba4cd97d77ca96f0098cc28218c3200bda
andrasgotts <andras.gotts@outlook.com>
2025-09-06 14:05:27 +0100

    nothing

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { motion } from 'motion/react'
 
 function App() {
   const [count, setCount] = useState(0)
